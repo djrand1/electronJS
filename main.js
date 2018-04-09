5e1d39a0bd364ae62c2f472c127ca498bf613df8
dacdao1 <ddao3@student.gsu.edu>
2018-04-09 10:06:02 -0400

    second commit

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ const electron = require('electron')
 const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
-require('electron-reload')(__dirname);
 const path = require('path')
 const url = require('url')
+require('electron-reload')(__dirname);
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
--- renderer.js
+++ renderer.js
@@ -1,8 +1,50 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
-//Variable for the modal by Id
-//Variable for the modal by Id
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current control button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
 var modal = document.getElementById('loginPage');
 //Variable for the button to open modal
 var modalBut = document.getElementById('loginOpen');
@@ -34,9 +76,15 @@ function clickOutside(e){
 }
 
 function checkInfoLogin(form) {
+  var attemp=3;
   var errors1=[];
   var email1=form.loginEmail.value;
   var password1=form.loginPass.value;
+
+  if(attemp==0){
+
+    alert("Too many failed attempts. Please contact administrator.");
+  }
 
   if(email1==""){
     errors1.push('email1 field is empty.');
@@ -53,6 +101,7 @@ function checkInfoLogin(form) {
      for(var i = 0;i< errors1.length;i++){
        message1+=errors1[i] + "\n";
      }
+     attemp--;
      alert(message1);
      return false;
    }
